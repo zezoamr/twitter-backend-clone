c0e55b480d1f2e46aee3a845169dcac632924a8f
ziad amr shalaby <zezoamr@yandex.com>
2023-07-12 04:11:33 +0300

    Update User.js

--- Src/controllers/User.js
+++ Src/controllers/User.js
@@ -1,4 +1,9 @@
 const auth = require("../middleware/auth");
+const lodash = require('lodash');
+
+//to do
+// upload avatar and banner
+// search / by user tag /get user by id
 
 
 const userBan = async (req, auth, res) => { // doesn't delete the account just locks out everything //requires admin user
@@ -45,33 +50,42 @@ const userDelete = async (req, auth, res) => { // deletes user and his tweets
     }
 }
 
-const userFollow = async (req, auth, res) => {}
+const userFollowUnFollow = async (req, auth, res) => {
+    try { // you can not follow your self
+        if (req.params.id.toString() == req.user._id.toString()) {
+            throw new Error("you can not follow your self ")
+        }
 
-const userUnFollow = async (req, auth, res) => {}
+        // find user
+        const user = await User.findById(req.params.id);
+        // if no user
+        if (! user) {
+            throw new Error("no user found");
+        }
+        // *check if you already follow the user
+        const isfollowed = req.user.following.some((followed) => followed.followingId.toString() === req.params.id)
 
-const userGetFollowers = async (req, auth, res) => {
-    try {
-        const sort = [{
-                createdAt: -1
-            }];
-        const limit = req.query.limit ? parseInt(req.query.limit) : 30;
-        const skip = req.query.skip ? parseInt(req.query.skip) : 0;
-        const user = await User.findOne({_id: req.params.id});
-        await user.populate({
-            path: "follower",
-            select: "_id screenName tag followercount followingcount profileAvater.url Biography",
-            options: {
-                limit: parseInt(limit), // to limit number of user
-                skip: parseInt(skip),
-                sort
-            }
-        });
+        if (isfollowed) { // *add to user following
+            req.user.following = req.user.following.filter((follow) => {
+                return follow.followingId != req.params.id;
+            })
+            req.user.followingcount -= 1
+            await req.user.save()
+            user.followercount -= 1
+            await user.save()
+        } else { // *add to user following
+            req.user.following = req.user.following.concat({followingId: user._id})
+            req.user.followingcount ++
+            await req.user.save()
+            user.followercount ++
+            await user.save()
+        }
 
-        return res.status(200).send(user.follower)
     } catch (e) {
-        res.status(404).send()
+        res.status(400).send({error: e.toString()})
     }
 }
+
 
 const userGetFollowing = async (req, auth, res) => {
     try {
@@ -83,7 +97,7 @@ const userGetFollowing = async (req, auth, res) => {
         const user = await User.findOne({_id: req.params.id});
         await user.populate({
             path: "following",
-            select: "_id screenName tag followercount followingcount profileAvater.url Biography",
+            select: "_id screenName tag followercount followingcount profileAvater Biography",
             options: {
                 limit: parseInt(limit), // to limit number of user
                 skip: parseInt(skip),
@@ -96,34 +110,87 @@ const userGetFollowing = async (req, auth, res) => {
     }
 }
 
-/**const userIsFollowingOther = async (req, auth, res) => {
- // to check if you follow the user or not
+const userGetFollowers = async (req, auth, res) => {
+    try {
+        const sort = [{
+                createdAt: -1
+            }];
+        const limit = req.query.limit ? parseInt(req.query.limit) : 30;
+        const skip = req.query.skip ? parseInt(req.query.skip) : 0;
+        const user = await User.findOne({_id: req.params.id});
+        await user.populate({
+            path: "follower",
+            select: "_id screenName tag followercount followingcount profileAvater Biography",
+            options: {
+                limit: parseInt(limit), // to limit number of user
+                skip: parseInt(skip),
+                sort
+            }
+        });
+        // to check if you follow the user or not
         if (! user.follower.length < 1) {
             user.follower = user.follower.map((follow) => {
-                const isfollowed = req.user.following.some((followed) => followed.followingId.toString() == follow._id.toString())
+                const isfollowed = req.user.following.some((followed) => followed.followingId.toString() == follow._id.toString());
                 delete follow._doc.following;
                 if (isfollowed) {
                     userFollower = {
                         ...follow._doc,
                         isfollowing: true
-                    }
+                    };
+                    return userFollower;
                 } else {
                     userFollower = {
                         ...follow._doc,
                         isfollowing: false
-                    }
+                    };
+                    return userFollower;
                 }
-                return userFollower
-            })
+            });
         }
+
+        res.send(user.follower);
+    } catch (e) {
+        res.status(400).send({error: e.toString()});
     }
- */
+}
+
+const userSuggestedAccounts = async (req, auth, res) => {
+    try {
+
+        const followingsId = req.user.following.map((user) => {
+            return user.followingId;
+        })
+
+        followingsId.push(req.user._id)
+        let suggestedAccounts = await User.find({
+            _id: {
+                $nin: followingsId
+            },
+            isPrivate: false
+        })
+        suggestedAccounts = lodash.sampleSize(suggestedAccounts, 4)
+        
+        res.send({suggestedAccounts})
+
+    } catch (e) {
+        res.status(400).send({error: e.toString()});
+    }
+}
+
+const userGetMe = async (req, auth, res) => {
+    try {
+      res.send(req.user);
+    } catch (e) {
+      res.status(400).send({ error: e.toString() });
+    }
+  }
 
 module.exports = {
     userBan,
     userDelete,
-    userFollow,
-    userUnFollow,
+    userFollowUnFollow,
     userGetFollowers,
-    userGetFollowing
+    userGetFollowing,
+    userSuggestedAccounts,
+    userGetMe
 }
